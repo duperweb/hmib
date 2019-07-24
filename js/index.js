ba13023dcbdf69c225ad34b5651fac978e3d9635
duperweb <duperweb@gmail.com>
2019-07-23 20:31:26 -0400

    adding side menu, with some js code

--- js/index.js
+++ js/index.js
@@ -31,3 +31,14 @@ function showOn() {
     }
 
 }
+
+
+    function openSlideMenu(){
+      document.getElementById('side-menu').style.width = '250px';
+     // document.getElementById('main').style.marginLeft = '250px';
+    }
+
+    function closeSlideMenu(){
+      document.getElementById('side-menu').style.width = '0';
+      //document.getElementById('main').style.marginLeft = '0';
+    }
